47d36c9bfb4cc1146ce0c809d19a4ca88d7c3c06
Fuyouying <Fuyouying@qq.com>
2022-04-30 10:11:34 +0800

    master commit3

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
     }
 }
